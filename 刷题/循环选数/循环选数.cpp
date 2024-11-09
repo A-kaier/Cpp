3254fe68974a0694ecb6b853f053844bb73675b9
xu-akaer <3135866133@qq.com>
2024-11-09 11:21:07 +0800

    循环选数

--- 刷题/循环选数/循环选数.cpp
+++ 刷题/循环选数/循环选数.cpp
@@ -8,26 +8,30 @@ using namespace std;
 // 2）输出1到100之间（包括1和100）的能整除5的数字。
 
 //	while循环
-//int main()
-//{
-//
-//	int i = 0; // 存放键盘输入的整数 
-//	int tmp = 0; // 存放数据之和 
-//	int num = 0; // 存放有效数据  
-//	while(true)
-//	{
-//		cout<<"请输入值：";cin>>i;
-//		if(i == 0) break;
-//		if(i<1 && i>100) continue; // continue跳过本次，往下继续
-//		// 整数在1~100之间的有效数据 
-//		num++;
-//		tmp += i;
-//	} 
-//	cout<<endl;
-//	cout<<"有效数据的个数："<<num<<",有效数据的总合："<<tmp<<endl;
-//	
-//	return 0;
-//}
+/*
+
+
+int main()
+{
+
+	int i = 0; // 存放键盘输入的整数 
+	int tmp = 0; // 存放数据之和 
+	int num = 0; // 存放有效数据  
+	while(true)
+	{
+		cout<<"请输入值：";cin>>i;
+		if(i == 0) break;
+		if(i<1 && i>100) continue; // continue跳过本次，往下继续
+		// 整数在1~100之间的有效数据 
+		num++;
+		tmp += i;
+	} 
+	cout<<endl;
+	cout<<"有效数据的个数："<<num<<",有效数据的总合："<<tmp<<endl;
+	
+	return 0;
+}
+*/
 
 // 整除5
 int main()
