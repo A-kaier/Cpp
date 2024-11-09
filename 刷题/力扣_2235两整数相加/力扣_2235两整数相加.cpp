5154c1dd36b86c15deccdb53344a798667c9480f
xu-akaer <3135866133@qq.com>
2024-11-09 14:26:03 +0800

    力扣_2235两整数相加

---
+++ 刷题/力扣_2235两整数相加/力扣_2235两整数相加.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//给你两个整数 num1 和 num2，返回这两个整数的和。
+int num(int a,int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<num(a,b);
+
+	return 0;
+}
+
+
