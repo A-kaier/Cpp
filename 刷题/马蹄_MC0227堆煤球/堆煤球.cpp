5422c80f0341088208e1cd706c2042f9d8d99f45
xu-akaer <3135866133@qq.com>
2024-11-09 21:41:40 +0800

    马蹄_MC0227堆煤球

--- 刷题/马蹄_MC0227堆煤球/堆煤球.cpp
+++ 刷题/马蹄_MC0227堆煤球/堆煤球.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 int main()
 {
-	
+	int a,b,num = 0;
+	cin>>a>>b;
+	for(int i = a;i <= b;i++)
+		if(i % 8) // 取整除 ,布尔表达式 
+			num += (1 + i) * i / 2;
+	cout<<num;
+
 
 	return 0;
 }
