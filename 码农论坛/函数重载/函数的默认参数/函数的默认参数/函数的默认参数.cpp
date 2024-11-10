a56de7c4647e62b01f2be74451062acaf98f5673
A-kaier <3135866133@qq.com>
2024-11-10 15:14:02 +0800

    码农论坛/函数重载/函数的默认参数

---
+++ 码农论坛/函数重载/函数的默认参数/函数的默认参数/函数的默认参数.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+
+//  语法：返回值 函数名(数据类型 参数 = 值, 数据类型 参数 = 值, ……);
+
+void func(int a, const string& name = "西施", const string& message = "我喜欢你")
+{
+	cout << "亲爱的" << name << a << message << endl;
+}
+
+int main()
+{
+	func(3, "冰冰", "我是一只傻傻鸟");
+	func(5);
+
+	return 0;
+}
