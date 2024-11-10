e40ea50b5396a4d681338c0447ae5ea8b19da0f6
A-kaier <3135866133@qq.com>
2024-11-10 15:45:14 +0800

    码农论坛\运算符重载\重载关系运算符

---
+++ 码农论坛/运算符重载/重载关系运算符/重载关系运算符/重载关系运算符.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+/*重载关系运算符（ == 、 != 、 > 、 >= 、 < 、 <= ）用于比较两个自定义数据类型的大小。
+	可以使用非成员函数和成员函数两种版本，建议采用成员函数版本*/
+
+class CGirl
+{
+	
+};
+
+int main()
+{
+
+
+	return 0;
+}
