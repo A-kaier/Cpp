e81b88ce225dbe1921035336db297e1a32b9af97
A-kaier <3135866133@qq.com>
2024-11-10 15:09:11 +0800

    2024_9_26/用new动态创建一维数组

---
+++ 码农论坛/2024_9_26/用new动态创建一维数组/用new动态创建一维数组/用new动态创建一维数组.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//动态创建一维数组的语法：数据类型* 指针 = new 数据类型[数组长度];
+	//释放一维数组的语法：delete[] 指针;
+
+	int* arr = new int[8];
+	for (int i = 0; i < 8; i++)
+	{
+		arr[i] = 100 + i;
+		cout << i <<"   " << arr + i;
+
+	}
+	delete[] arr;
+	return 0;
+}
