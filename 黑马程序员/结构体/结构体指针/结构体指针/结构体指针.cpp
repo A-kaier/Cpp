5cfbb5d9611b2f0f1db8b1949413be4d7d171ba7
A-kaier <3135866133@qq.com>
2024-11-10 15:52:49 +0800

    结构体指针

---
+++ 黑马程序员/结构体/结构体指针/结构体指针/结构体指针.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+
+struct myStruct
+{
+	int age;
+	int qian;
+	int shengao;
+};
+
+int main()
+{
+	struct myStruct arr = { 18,88888,191 };
+	myStruct* p = &arr;
+	p->qian = 99999;
+
+	cout
+		<< "年龄：" << p->age
+		<< "，余额：" << p->qian
+		<< "，身高：" << p->shengao
+		<< endl;
+
+	return 0;
+}
