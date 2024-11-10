900ede6c3bb2919174725080dbee0e83bdff58b9
A-kaier <3135866133@qq.com>
2024-11-10 15:09:28 +0800

    2024_9_28/C风格的字符串

---
+++ 码农论坛/2024_9_28/一维数组的排序 qsort/一维数组的排序 qsort/一维数组的排序qsort.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+// gsort0函数用于对各种数据类型的数组进行排序。
+
+// 函数的原型：
+// 第一个参数：数组的起始地址。
+// 第二个参数：数组元素的个数（数组长度）。
+// 第三个参数：数组元素的大小（sizeof(数组的数据类型）。
+//	第四个参数：回调函数的地址。
+//	回调函数决定了排序的顺序，声明如下：
+//	nt comar(const vid * 1, const vid*)
+//	1）如果函数的返回值 < 0，那么 p1所指向元素会被排在 p2 所指向元素的前面。
+//	2）如果函数的返回值 = 0，那么 p1 所指向元素与 p2 所指向元素的顺序不确定。
+//	3）如果函数的返回值>0，那么 p1 所指向元素会被排在 p2 所指向元素的后面。
+//	void qsort(void* base, sizet nmemb, size_t size, int (*compar)(const void*, const void*)
+
+// 升序的回调函数
+int num(const void* p1, const void* p2)
+{
+	return *(int*)p1 - *(int*)p2;
+}
+// 降序的回调函数
+int num2(const void* p1, const void* p2) {
+	return *(int*)p2 - *(int*)p1;
+}
+
+int main()
+{
+	int a[8] = { 4,1,7,5,9,3,8,2 };
+	qsort(a, sizeof(a) / sizeof(int), sizeof(int), num); // 对数组 a 进行升序
+	for (int i = 0; i < 8; i++)
+	{
+		cout << "a[" << i << "] = " << a[i] << endl;
+	}
+	cout << endl; // 换行
+	qsort(a, sizeof(a) / sizeof(int), sizeof(int), num2);  // 对数组 a进行降序排序
+	for (int i = 0; i < 8; i++)
+	{
+		cout << "a[" << i << "] = " << a[i] << endl;
+	}
+
+	return 0;
+}
---
+++ 码农论坛/2024_9_28/一维数组的查找-折半查找/一维数组的查找-折半查找/一维数组的查找-折半查找.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+// 1.函数声明，接受一个整数数组arr，数组长度len，以及要查找的键值key,失败返回-1;
+int search(int arr[], int key, int len)
+{
+    // 2.初始化三个变量，low为查找区间的第一个元素的下标，high为最后一个元素的下标，mid为中间点。
+    int low = 0, high = len - 1, mid;
+
+    // 3.当low小于或等于high时，继续查找;
+    while (low <= high)
+    {
+        // 4.计算中间点的位置。
+        mid = (low + high) / 2;
+        // 5.如果中间点的值等于要查找的键值，返回中间点的索引
+        if (arr[mid] == key) return mid;
+        // 6.如果中间点的值大于键值，将查找区间缩小到前半部分。
+        else if (arr[mid] > key) high = mid - 1;
+        // 7.如果中间点的值小于键值，将查找区间缩小到后半部分。
+        else low = mid + 1;
+    }
+    // 8.如果循环结束都没有找到键值，返回-1表示查找失败。
+    return -1;
+}
+
+int main()
+{
+    // 9.定义并初始化一个已排序的数组。
+    int a[10] = { 1,3,5,7,9,11,13,15,17,19 };
+    // 10.调用search函数查找值19，如果返回值大于或等于0，表示找到了，打印成功消息。
+    if (search(a, 7, 19) >= 0) cout << "在数组a中查找19成功\n" << endl;
+    // 11.如果没有找到，打印失败消息。
+    else cout << "打印消息失败" << endl;
+
+    return 0;
+}
+
+
