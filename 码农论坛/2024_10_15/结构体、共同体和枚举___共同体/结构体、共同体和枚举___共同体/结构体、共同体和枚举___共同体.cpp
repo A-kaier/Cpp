c67b5006fe1d05e70fffd739a368430158c435fb
A-kaier <3135866133@qq.com>
2024-11-10 15:12:30 +0800

    2024_10_15/结构体、共同体和枚举___共同体

---
+++ 码农论坛/2024_10_15/结构体、共同体和枚举___共同体/结构体、共同体和枚举___共同体/结构体、共同体和枚举___共同体.cpp
@@ -0,0 +1,81 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+/*union 共同体名
+{
+	成员一的数据类型  成员名一;
+	成员二的数据类型  成员名二;
+	成员三的数据类型  成员名三;
+	......
+		成员n的数据类型  成员名n;
+};*/
+
+#if 0
+union  udata       // 声明共同体udata。
+{
+	int		a;
+	double b;
+	char		 c[25];
+};
+
+int main()
+{
+	udata data;     // 定义共同体变量。
+	cout << "sizeof(data)=" << sizeof(data) << endl;
+
+	cout << "data.a的地址是：" << (void*)&data.a << endl;
+	cout << "data.b的地址是：" << (void*)&data.b << endl;
+	cout << "data.c的地址是：" << (void*)&data.c << endl;
+	cout << endl;
+
+	data.a = 3;
+	data.b = 8.8;
+	strcpy(data.c, "我是一只傻傻鸟。");
+	cout << "data.a的地址是：" << data.a << endl;
+	cout << "data.b的地址是：" << data.b << endl;
+	cout << "data.c的地址是：" << data.c << endl;
+
+	return 0;
+}
+#endif // 0
+
+#if 1
+struct st_girl       // 声明超女结构体。
+{
+	int no;             // 超女编号。
+
+	union              // 声明匿名共同体。
+	{
+		int        a;
+		double b;
+		char     c[21];
+	};
+};
+
+int main()
+{
+	struct st_girl girl;
+
+	cout << "girl.a的地址是：" << (void*)&girl.a << endl;
+	cout << "girl.b的地址是：" << (void*)&girl.b << endl;
+	cout << "girl.c的地址是：" << (void*)&girl.c << endl;
+	cout << endl;
+
+	girl.a = 3;
+	girl.b = 8.8;
+	strcpy(girl.c, "我是一只傻傻鸟。");
+	cout << "girl.a=" << girl.a << endl;
+	cout << "girl.b=" << girl.b << endl;
+	cout << "girl.c=" << girl.c << endl;
+	cout << endl;
+
+	girl.a = 3;
+	cout << "girl.a=" << girl.a << endl;
+	girl.b = 8.8;
+	cout << "girl.b=" << girl.b << endl;
+	strcpy(girl.c, "我是一只傻傻鸟。");
+	cout << "girl.c=" << girl.c << endl;
+}
+
+#endif // 0
---
+++ 码农论坛/2024_10_15/结构体、共同体和枚举___枚举/结构体、共同体和枚举___枚举/结构体、共同体和枚举___枚举.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+using namespace std;
+
+/*枚举是一种创建符号常量的方法。
+枚举的语法：
+	enum 枚举名 { 枚举量1, 枚举量2, 枚举量3, ......, 枚举量n };
+例如：
+	enum colors { red, yellow, blue };*/
+
+int main()
+{
+	// 创建枚举类型colors
+	enum en_a1
+	{
+		red = 0, yellow = 1, blue = 2, other = 3
+	};
+	// 创建枚举变量，并赋初始值。
+
+
+	en_a1 cc = red;
+	cout
+		<< "red=" << red
+		<< ",yellow=" << yellow
+		<< ",blue=" << blue
+		<< ",other=" << other
+		<< endl;
+
+	switch (cc)
+	{
+	case red:
+		cout << "红色。\n";
+		break;
+	case yellow:
+		cout << "黄色。\n";
+		break;
+	case blue:
+		cout << "蓝色。\n";
+		break;
+	default:
+		cout << "未知。\n";
+		break;
+	}
+
+	return 0;
+}
