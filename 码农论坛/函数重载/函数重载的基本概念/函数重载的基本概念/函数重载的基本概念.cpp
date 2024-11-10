4518fd875378b5594aa714153f1d141146a3d24a
A-kaier <3135866133@qq.com>
2024-11-10 15:43:51 +0800

    码农论坛\函数重载\函数重载的基本概念

---
+++ 码农论坛/函数重载/函数重载的基本概念/函数重载的基本概念/函数重载的基本概念.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+
+void func1(int& a, int& b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void func2(string& a, string& b)
+{
+	string tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int main()
+{
+	int a = 3, b = 6;
+	func1(a, b);
+	cout << "a + b = " << a + b << endl;
+
+	string c = "西施";
+	string d = "西瓜";
+	cout << "c = " << c << "， d = " << d << endl;
+
+	string e = "西施";
+	int f = 10;
+	cout << "c = " << c << "， f = " << f << endl;
+
+
+	return 0;
+}
