cfde5844916a55f47797d9a6425cdd0de58479c0
A-kaier <3135866133@qq.com>
2024-11-10 15:53:00 +0800

    结构体中const使用场景

---
+++ 黑马程序员/结构体/结构体中const使用场景/结构体中const使用场景/结构体中const使用场景.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+struct student
+{
+	string name;
+	int age;
+	int score;
+};
+
+void coutarrstu(const struct student* s) // 加指针是为了减少内存，一个指针只有四个字节
+{
+	s->age = 90; // 加了const就不能更改值
+	cout << "姓名：" << s->name << "，年龄：" << s->age << "，分数：" << s->score << endl;
+};
+
+int main()
+{
+	student s = { "张三",18,100 };
+	coutarrstu(&s);
+
+	return 0;
+}
