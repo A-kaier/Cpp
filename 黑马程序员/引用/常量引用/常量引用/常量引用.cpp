9565910a3c8262b594d92b8eb7e92bcd70bcb676
A-kaier <3135866133@qq.com>
2024-11-20 22:39:45 +0800

    常量引用

---
+++ 黑马程序员/引用/常量引用/常量引用/常量引用.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//引用使用的场景，通常用来修饰形参
+void showvalue(const int& v)
+{
+	// v = 1000;
+	cout << "v = " << v << endl;
+}
+
+int main()
+{
+	// int& ref = 10;  // 引用本身需要一个合法的内存空间，因此这行错误
+	//加入const就可以了，编译器优化代码，int temp = 10; const int& ref = temp;
+	const int& ref = 10;
+
+	//ref = 100;  //加入const后不可以修改变量
+
+	cout << "ref = " << ref << endl;
+
+	// 函数中利用常量引用防止误操作修改实参
+	int a = 100;
+	showvalue(a);
+	cout << "a = " << a << endl;
+
+	system("pause");
+	return 0;
+}
